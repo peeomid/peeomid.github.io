2a5b5dd5cae7c1edd75e4ec2b1f2145b13c3d271
Luan <lihavim@gmail.com>
2018-01-27 01:54:05 +0700

    Update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Tips To A Start Django Project",
+        "title": "Tips To Start A Django Project",
         "excerpt":"Hi there! This is an article on Django development","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/2018-01-26-tips-to-a-start-django-project/",
+        "url": "http://localhost:4000/blog/2018-01-26-tips-to-start-a-django-project/",
         "teaser":null},]
