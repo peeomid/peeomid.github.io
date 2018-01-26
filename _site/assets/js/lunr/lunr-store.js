986ca3e71d2f787832247ac5939b3ebabfa4345b
Luan <lihavim@gmail.com>
2018-01-26 18:20:57 +0700

    change layout

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Tips To A Start Django Project",
+        "excerpt":"Hi there! This is an article on Django development","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2018-01-26-tips-to-a-start-django-project/",
+        "teaser":null},]
