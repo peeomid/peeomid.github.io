c93496c50ae5ea72a6ff9653bcebb3708c61f6ab
Luan <lihavim@gmail.com>
2018-01-27 12:40:22 +0700

    fixed untracked files

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Tips To Start A Django Project",
-        "excerpt":"Hi there! This is an article on Django development","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/blog/2018-01-26-tips-to-start-a-django-project/",
-        "teaser":null},]
